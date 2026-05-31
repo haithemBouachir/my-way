c580137e4f63f96fdb74655cd2c70d05c66e0cbf
haithemBouachir <haithembouachir@gmail.com>
2026-05-31 11:08:43 +0100

    terraform: always create managed security group for EC2 SSH access

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,18 +21,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_security_groups" "existing_app_sg" {
-  filter {
-    name   = "group-name"
-    values = ["${var.project_name}-security-group"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -49,8 +37,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_security_group" "app_sg" {
-  count       = length(data.aws_security_groups.existing_app_sg.ids) == 0 ? 1 : 0
-  name        = "${var.project_name}-security-group"
+  name_prefix = "${var.project_name}-sg-"
   description = "Allow SSH, HTTP and HTTPS access"
   vpc_id      = data.aws_vpc.default.id
 
@@ -90,15 +77,11 @@ resource "aws_security_group" "app_sg" {
   )
 }
 
-locals {
-  app_security_group_id = length(data.aws_security_groups.existing_app_sg.ids) > 0 ? data.aws_security_groups.existing_app_sg.ids[0] : aws_security_group.app_sg[0].id
-}
-
 resource "aws_instance" "app" {
   ami                         = var.ami_id != "" ? var.ami_id : data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  vpc_security_group_ids      = [local.app_security_group_id]
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
 
   root_block_device {
